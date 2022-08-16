fbfe488b4968b5a4268c1642596042549e573c3d
Constantine Krotov <ckwebmast@gmail.com>
2022-08-17 01:44:25 +0300

    completed lesson11

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const btn = document.querySelector('#btn')
+const range = document.querySelector('#range')
+const span = document.querySelector('#range-span')
+
+e_btn.style = 'display: none'
+span.textContent = 50 + '%'
+
+
+const rangeLogger = function (event) {
+  span.textContent = event.target.value + '%'
+  circle.style.height = `${range.value}px`
+}
+
+const logger = function (event) {
+  square.style.background = `${text.value}`
+}
+
+
+btn.addEventListener('click', logger)
+range.addEventListener('input', rangeLogger)
+
+
+
+
+
+
 
 
 
